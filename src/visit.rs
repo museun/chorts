908f6969ed224574b9a511395c0e31a3a9715568
museun <museun@users.noreply.github.com>
2025-03-28 14:08:26 -0400

    add partition highlight

--- src/visit.rs
+++ src/visit.rs
@@ -142,4 +142,74 @@ impl Highlight<'_> {
     }
 }
 
+#[derive(Copy, Clone, Debug, PartialEq)]
+pub struct Partition<'a> {
+    pub head: &'a str,
+    pub middle: &'a str,
+    pub tail: &'a str,
+}
+
+pub fn partition<'a, 'b>(
+    spans: &'b [Highlight<'a>],
+) -> impl Iterator<Item = Partition<'a>> + use<'a, 'b>
+where
+    'b: 'a,
+{
+    let mut iter = spans.iter().enumerate();
+    let mut left_pad = 0;
+
+    std::iter::from_fn(move || loop {
+        let (i, span) = iter.next()?;
+        if span.data.trim_start().is_empty() {
+            continue;
+        }
+
+        if i == 0 {
+            let s = span.data.trim_start();
+            left_pad = span.data.len() - s.len();
+        }
+
+        let start = span.start.saturating_sub(left_pad + 1);
+        let end = span.end.saturating_sub(left_pad + 1);
+
+        let start = str_indices::chars::from_byte_idx(&span.data, start);
+        let end = str_indices::chars::from_byte_idx(&span.data, end);
+
+        let text = &span.data[left_pad..];
+        let start = floor_char_boundary(text, start);
+        let end = ceil_char_boundary(text, end);
+
+        break Some(Partition {
+            head: &text[..start],
+            middle: &text[start..end],
+            tail: &text[end..],
+        });
+    })
+}
+
+fn floor_char_boundary(str: &str, index: usize) -> usize {
+    if index >= str.len() {
+        return str.len();
+    }
+
+    let start = index.saturating_sub(3);
+    let next = str.as_bytes()[start..=index]
+        .iter()
+        .rposition(|&b| (b as u8) >= -0x40)
+        .unwrap();
+    start + next
+}
+
+fn ceil_char_boundary(str: &str, index: usize) -> usize {
+    if index > str.len() {
+        return str.len();
+    }
+
+    let end = (index + 4).min(str.len());
+    str.as_bytes()[index..end]
+        .iter()
+        .position(|&b| (b as u8) >= -0x40)
+        .map_or(end, |pos| pos + index)
+}
+
 // TODO relocate the highlight
